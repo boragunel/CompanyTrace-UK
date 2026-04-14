a47d8e212b7d437350153d81cf2dbaeb4b378b51
boragunel <bmg24@sussex.ac.uk>
2026-04-14 13:02:57 +0100

    auto deploy

---
+++ deploy.py
@@ -0,0 +1,9 @@
+import subprocess
+
+print("Pushing to GitHub...")
+subprocess.run(["git", "add", "Website.py", "deploy.py"])
+subprocess.run(["git", "commit", "-m", "auto deploy"])
+subprocess.run(["git", "push"])
+
+print("Done! Render will automatically deploy in ~1 minute.")
+print("Your site: https://companytrace-uk.onrender.com")
